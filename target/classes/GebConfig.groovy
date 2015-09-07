5777911cb07fd8cd02fdfd6f3ab280e36feca676
m-kubota <m-kubota@iid.co.jp>
2015-09-07 18:09:01 +0900

    target

--- target/classes/GebConfig.groovy
+++ target/classes/GebConfig.groovy
@@ -1,10 +1,6 @@
 //choose "htmlunit", "firefox", "ie", "chrome"
 driver = "firefox"
 
-//reports setting
-reportsDir = "target/geb-reports"
-reportOnTestFailureOnly = true
-
 //chrome - http://chromedriver.storage.googleapis.com/index.html
 System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe")
 
